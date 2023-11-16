48791faa0f5a44fb143b389826ce2136dedbce99
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-16 15:25:52 +0100

    es c

--- snack-9/js/scrpt.js
+++ snack-9/js/scrpt.js
@@ -1,3 +1,12 @@
 const dueNumeri = parseInt( prompt("inserisci un numero di 2 cifre"));
 const quattroNumeri = parseInt( prompt("inserisci un numero di 4 cifre"));
 
+if(dueNumeri <= 10 && dueNumeri >= 99 && quattroNumeri >= 1000){
+    let somma = dueNumeri;
+
+} else if (dueNumeri <= quattroNumeri){
+
+    somma *= 2;
+    console.log('il risultato è' + somma)
+
+}
