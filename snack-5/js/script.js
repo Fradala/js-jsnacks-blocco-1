17a85cc84291e1671d9e3cdcd92361f631f40911
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-16 12:36:37 +0100

    snack fatto

---
+++ snack-5/js/script.js
@@ -0,0 +1,16 @@
+const primaParola = prompt ('inserisci una parola')
+const secondaParola = prompt ('inserisci una parola')
+
+
+
+
+
+
+if( parolaLunga = primaParola.length > secondaParola.length){
+   
+    console.log(secondaParola, primaParola)
+} else {
+   
+    console.log(primaParola, secondaParola)
+}
+
