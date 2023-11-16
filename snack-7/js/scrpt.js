6d086be5220404fc1948a10a79bb8695677a9b27
Francesco.dalascio <francesco.dalascio.2002@gmail.com>
2023-11-16 13:46:27 +0100

    iniziato snack-7

---
+++ snack-7/js/scrpt.js
@@ -0,0 +1,8 @@
+const numeri = [];
+
+const primoNumeroUtente = parseInt( prompt ('inserisci un numero'));
+const secondoNumeroUtente = parseInt( prompt ('inserisci un numero'));
+const terzoNumeroUtente = parseInt( prompt ('inserisci un numero'));
+const quartoNumeroUtente = parseInt( prompt ('inserisci un numero'));
+const quintoNumeroUtente = parseInt( prompt ('inserisci un numero'));
+const sestoNumeroUtente = parseInt( prompt ('inserisci un numero'));
